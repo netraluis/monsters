2a75110be822f93c6cc3e22d799444466dfd9630
funzza <netraluis@gmail.com>
2020-02-01 18:47:46 +0100

    Updates

---
+++ precache-manifest.a88720e897dfbe33ab0ef375bdad0322.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60d68dddc805e6fd68a0b40ff4719a3d",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "5b40f48832dff9828d47",
+    "url": "/monsters/static/css/main.9fdb309c.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b40f48832dff9828d47",
+    "url": "/monsters/static/js/main.634ce076.chunk.js"
+  },
+  {
+    "revision": "27edc721a646a18481ad",
+    "url": "/monsters/static/js/runtime-main.9b332845.js"
+  }
+]);
